4713b9869cd5cc77e66222282f87be90ada13540
Yashwanth Kumar <ykms1@st-andrews.ac.uk>
2023-03-03 14:25:01 +0000

    #4 Test cases for API

--- src/main/java/cs5031/bus_travel_planner/api/IndexController.java
+++ src/main/java/cs5031/bus_travel_planner/api/IndexController.java
@@ -4,15 +4,14 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.stereotype.Controller;
+import org.springframework.http.MediaType;
 
 @Controller
 public class IndexController {
 
-    @RequestMapping(method = RequestMethod.GET, value = "/")
-    public String welcome() {
-        return "index.html";
-    }
+    // Model model; //Object for model to call search function
 }
 
 	
--- src/main/resources/static/client.js
+++ src/main/resources/static/client.js
@@ -2,45 +2,53 @@
 
 
 
-function Display() {
-    if (document.getElementById("oneWay").checked) {
-        console.log("oneway");
-        console.log(document.getElementById("fromDate").value);
-    } else if (document.getElementById("twoWay").checked) {
-        console.log("twoWay");
-        console.log(document.getElementById("fromDate").value);
-    }
-}
+// function Display() {
+//     if (document.getElementById("oneWay").checked) {
+//         console.log("oneway");
+//         console.log(document.getElementById("fromDate").value);
+//     } else if (document.getElementById("twoWay").checked) {
+//         console.log("twoWay");
+//         console.log(document.getElementById("fromDate").value);
+//     }
+// }
 
-function EnableReturnDate() {
-    if (document.getElementById("returnDate").hasAttribute("disabled")) {
-        document.getElementById("returnDate").removeAttribute("disabled")
-    }
-}
+// function EnableReturnDate() {
+//     if (document.getElementById("returnDate").hasAttribute("disabled")) {
+//         document.getElementById("returnDate").removeAttribute("disabled")
+//     }
+// }
 
-function DisableReturnDate() {
-    if (!document.getElementById("returnDate").hasAttribute("disabled")) {
-        document.getElementById("returnDate").setAttribute("disabled", "disabled");
-    }
-}
+// function DisableReturnDate() {
+//     if (!document.getElementById("returnDate").hasAttribute("disabled")) {
+//         document.getElementById("returnDate").setAttribute("disabled", "disabled");
+//     }
+// }
 
 async function SearchBuses() {
 
     const from = document.getElementById("from").value;
     const to = document.getElementById("to").value;
-    const fromDate = document.getElementById("fromDate").value;
 
-    if (document.getElementById("oneWay").checked) {
-        console.log("oneway");
+    const weekdays = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+    const day = new Date(document.getElementById("day").value);
+    console.log(weekdays[day.getDay()]);
+
+    const time = (document.getElementById("day").value).substring(11);
+    console.log(time);
+
+    // if (document.getElementById("oneWay").checked) {
+    //     console.log("oneway");
+
         //API Request
-        const request = await fetch(`http://localhost:5000/buses?route=oneWay&from=${from}&to=${to}&fromDate=${fromDate}`, { method: "GET" });
+        const request = await fetch(`http://localhost:8080/buses?from=${from}&to=${to}&day=${day}&time=${time}`, { method: "GET" });
+        const response = await request.json();
+        console.log(response);
+    // } else if (document.getElementById("twoWay").checked) {
+    //     let toDate = document.getElementById("returnDate").value;
+    //     console.log("twoWay");
+    //     //API Request
+    //     const request = await fetch(`http://localhost:5000/buses?from=${from}&to=${to}&fromDate=${fromDate}&toDate=${toDate}`, { method: "GET" });
 
-    } else if (document.getElementById("twoWay").checked) {
-        let toDate = document.getElementById("returnDate").value;
-        console.log("twoWay");
-        //API Request
-        const request = await fetch(`http://localhost:5000/buses?route=twoWay&from=${from}&to=${to}&fromDate=${fromDate}&toDate=${toDate}`, { method: "GET" });
-
-    }
+    // }
 
 }

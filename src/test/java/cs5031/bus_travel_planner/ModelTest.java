19fa79418344dda309e1b24aff17c2f3acf679b9
Manish Mishra <mm551@st-andrews.ac.uk>
2023-03-05 15:57:04 +0000

    Added TC #3#4#5 to test reading json objects.

--- src/test/java/cs5031/bus_travel_planner/ModelTest.java
+++ src/test/java/cs5031/bus_travel_planner/ModelTest.java
@@ -5,6 +5,7 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 import org.json.JSONException;
 import org.json.JSONObject;
+import org.json.JSONArray;
 import java.nio.file.NoSuchFileException;
 import java.util.ArrayList;
 
@@ -19,6 +20,7 @@ public class ModelTest {
             testModel = new BusModel("src/test/resources/initialState.json");
         }
 
+    //TC1
     @Test
         public void shouldNotOpenJson() {
             NoSuchFileException thrown = 
@@ -27,6 +29,7 @@ public class ModelTest {
                         });
         }
 
+    //TC2
     @Test
         public void shouldOpenJson() throws IOException, JSONException {
             JSONObject result = null;
@@ -38,4 +41,53 @@ public class ModelTest {
 
             assertNotNull(result);
         }
+
+    //TC3
+    @Test
+        public void testReadStringJson() throws IOException, JSONException {
+            JSONObject result = null;
+            try {
+                result = BusModel.loadInitialState("src/test/resources/test.json");
+            }
+            catch (IOException | JSONException e) {
+            }
+            assertNotNull(result);
+            
+            String name = result.getString("name");
+            assertEquals("test", name);
+        }
+
+    //TC4
+    @Test
+        public void testReadArraySizeJson() throws IOException, JSONException {
+            JSONObject result = null;
+            try {
+                result = BusModel.loadInitialState("src/test/resources/test.json");
+            }
+            catch (IOException | JSONException e) {
+            }
+            assertNotNull(result);
+            
+            JSONArray array = result.getJSONArray("array");
+            assertEquals(3, array.length());
+        }
+
+    //TC5
+    @Test
+        public void testReadArrayValuesJson() throws IOException, JSONException {
+            JSONObject result = null;
+            try {
+                result = BusModel.loadInitialState("src/test/resources/test.json");
+            }
+            catch (IOException | JSONException e) {
+            }
+            assertNotNull(result);
+            
+            JSONArray array = result.getJSONArray("array");
+            for(int i = 0; i < array.length(); ++i) {
+                assertEquals(i+1, array.getJSONObject(i).getInt("num"));
+            }
+        }
+
+
 }
